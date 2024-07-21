e70d5fe05f0f286f740cf957215aaa1e3c32d773
ElisabettaMirti <belmirti@gmail.com>
2024-07-21 16:14:55 +0200

    popolato di nuovi dati con Faker

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,39 +5,40 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        $trains = [
-            [
-                "azienda" => "Trenitalia",
-                "stazione_di_partenza" => "Rimini",
-                "stazione_di_arrivo" => "Roma",
-                "orario_di_partenza" => "2024-07-18 06:00:00",
-                "orario_di_arrivo" => "2024-07-18 09:00:00",
-                "codice_treno" => 88679,
-                "numero_carrozze" => 9,
-                "in_orario" => 1,
-                "cancellato" => 0,
-            ]
-        ];
+        // $trains = [
+        //     [
+        //         "azienda" => "Trenitalia",
+        //         "stazione_di_partenza" => "Rimini",
+        //         "stazione_di_arrivo" => "Roma",
+        //         "orario_di_partenza" => "2024-07-18 06:00:00",
+        //         "orario_di_arrivo" => "2024-07-18 09:00:00",
+        //         "codice_treno" => 88679,
+        //         "numero_carrozze" => 9,
+        //         "in_orario" => 1,
+        //         "cancellato" => 0,
+        //     ]
+        // ];
 
-        foreach ($trains as $trainData) {
+        for ($i=0; $i < 100; $i++) {
             $train = new Train();
-            $train->azienda = $trainData['azienda'];
-            $train->stazione_di_partenza = $trainData['stazione_di_partenza'];
-            $train->stazione_di_arrivo = $trainData['stazione_di_arrivo'];
-            $train->orario_di_partenza = $trainData['orario_di_partenza'];
-            $train->orario_di_arrivo = $trainData['orario_di_arrivo'];
-            $train->codice_treno = $trainData['codice_treno'];
-            $train->numero_carrozze = $trainData['numero_carrozze'];
-            $train->in_orario = $trainData['in_orario'];
-            $train->cancellato = $trainData['cancellato'];
+            $train->azienda = $faker->words(2, true);
+            $train->stazione_di_partenza = $faker->words(4, true);
+            $train->stazione_di_arrivo = $faker->words(4, true);
+            $train->orario_di_partenza = $faker->unique()->dateTimeThisMonth();
+            $train->orario_di_arrivo = $faker->unique()->dateTimeThisMonth();
+            $train->codice_treno = $faker->randomNumber(5, true);
+            $train->numero_carrozze = $faker->randomNumber(2, true);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
             $train->save();
         }
     }
